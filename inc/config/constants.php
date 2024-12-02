f739ee656449545548394e36aab722f916a71ce5
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-12-02 23:06:36 +0100

    Update constants.php

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -5,16 +5,16 @@
 	
 	// Database parameters
 	// Data source name
-	define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
+	define('DSN', 'mysql:host=REPLACE_DB_HOST_ADDRESS;dbname=shop_inventory');
 	
 	// Hostname
-	define('DB_HOST', 'localhost');
+	define('DB_HOST', 'REPLACE_DB_HOST_ADDRESS');
 	
 	// DB user
-	define('DB_USER', 'inventoryUser');
+	define('DB_USER', 'REPLACE_DB_USER_NAME');
 	
 	// DB password
-	define('DB_PASSWORD', 'password');
+	define('DB_PASSWORD', 'REPLACE_DB_PASSWORD');
 	
 	// DB name
 	define('DB_NAME', 'shop_inventory');
